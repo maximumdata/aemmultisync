d63810e570caaabdabfc3753c6558eaff2281e5c
Mike Dettmer <mike@mikedettmer.com>
2018-05-21 14:35:46 -0400

    adds exclude flag

--- index.js
+++ index.js
@@ -2,13 +2,14 @@
 const util = require('./util');
 const aemsync = require('aemsync');
 const argv = require('yargs')
-              .usage('Usage: $0 -t [url to aem instance] -w [paths to watch (comma separated)] --verbose [verbose logging]')
+              .usage('Usage: $0 -t [url to aem instance] -w [paths to watch (comma separated)] -e [path globs to exclude (comma separated)] --verbose [verbose logging]')
               .demandOption(['t','w'])
-              .example('$0 -t http://admin:admin@example.gov:4503 -w ~/aemdir/frontend,~/aemdam --verbose')
+              .example('$0 -t http://admin:admin@example.gov:4503 -w ~/aemdir/frontend,~/aemdam -e **/node_modules/*,**/bower_components/* --verbose')
               .argv;
 
 let watchDirs = util.enforceArray(argv.w);
 let targets = util.enforceArray(argv.t);
+let excludes = util.enforceArray(argv.e);
 let verbose = argv.verbose;
 
 const interval = 300
@@ -23,7 +24,7 @@ watchDirs.forEach((workingDir) => {
   });
   let watcher = new aemsync.Watcher();
   pusher.start();
-  watcher.watch(workingDir, null, (localPath) => {
+  watcher.watch(workingDir, excludes, (localPath) => {
       pusher.enqueue(localPath);
   });
 })
